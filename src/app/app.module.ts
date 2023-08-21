6dde8a3f79c1dbba626b953f0df7ed11d8dc17fd
Bahrom Akramov <bahrom.akramov98@gmail.com>
2023-08-21 19:55:27 +0500

    test angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/Products.ts
@@ -0,0 +1,13 @@
+export class Products{
+    public Id? : number;
+    public Name? : string;
+    public SKU? : string;
+    public Category? : string;
+    public Brand? : string;
+    public Price? : number;
+    public Unit? : string;
+    public Quantity? : number;
+    public CreatedBy? : string;
+    public Picture? : string;
+
+}
